1c89377743c95b3863839b8db45af11c6b8a3a84
Prashant Bhure <Prashantbhure50@gmail.com>
2021-05-07 20:07:15 +0530

    [prasahnt] Game method is added

--- TicTacToeProblem/Program.cs
+++ TicTacToeProblem/Program.cs
@@ -8,12 +8,33 @@ namespace TicTacToeProblem
         {
             TicTacToe tic = new TicTacToe();
             char[] board = new char[10];
+            Random random = new Random();
             Console.WriteLine("Welcome to TicTacToe Game");
             tic.Board(board);
             tic.DisplayBoard(board);
-            Console.WriteLine("Enter a Choice X or O");
-            string pos = Console.ReadLine();
-            tic.PlayerPositionCheck(pos);
+            string Playerchoice = "X";
+            string computerChoice = "O";
+          
+          
+
+
+            Console.WriteLine("Enter a Choice Between  1-9");
+
+            while (true)
+            {
+                Console.WriteLine("   player Turn         ");
+                int position = Convert.ToInt32(Console.ReadLine());              
+                tic.PositionHolder(board, Playerchoice, position);
+                tic.DisplayBoard(board);
+                tic.checkWinner(board);
+                Console.WriteLine("   Computer Turn       ");
+                int computerTurn = random.Next(1,10);
+                tic.PositionHolder(board, computerChoice, computerTurn);
+                tic.DisplayBoard(board);
+                tic.checkWinner(board);
+            }
+
+
         }
     }
 }
--- TicTacToeProblem/TicTacToe.cs
+++ TicTacToeProblem/TicTacToe.cs
@@ -19,25 +19,132 @@ namespace TicTacToeProblem
             Console.WriteLine("  " + board[1] + "  |  " + board[2] + "  |   " + board[3] + "   ");
             Console.WriteLine("_____|_____|_____ ");
             Console.WriteLine("     |     |      ");
-            Console.WriteLine("  " + board[8] + "  |  " + board[7] + "  |   " + board[6] + "   ");
+            Console.WriteLine("  " + board[4] + "  |  " + board[5] + "  |   " + board[6] + "   ");
             Console.WriteLine("_____|_____|_____ ");
             Console.WriteLine("     |     |      ");
             Console.WriteLine("  " + board[7] + "  |  " + board[8] + "  |   " + board[9] + "   ");
             Console.WriteLine("     |     |      ");
         }
-        public void PlayerPositionCheck(string pos)
+        //public void PlayerChoice(string playerchoice )
+        //{
+        //    playerchoice = playerchoice.ToUpper();
+        //    if (playerchoice == "X")
+        //    {
+        //        Console.WriteLine("Player Choice Is {0}", playerchoice);
+        //        Console.WriteLine("Computer Choice Is O");
+                
+        //    }
+        //    else
+        //    {
+               
+        //        Console.WriteLine("Player Choice Is O");
+        //        Console.WriteLine("Computer Choice Is X");                
+        //    }
+        //}
+        public void PositionHolder(char[] board,string choice,int position)
         {
-            pos=pos.ToUpper();
-            if (pos == "X")
+            char symbol = ' ';
+            if (choice.Equals("X"))
             {
-                Console.WriteLine("Player Choice Is {0}",pos);
-                Console.WriteLine("Computer Choice Is O");
+                symbol = 'X';
+                
             }
-            else
+            else if (choice.Equals("O"))
             {
-                Console.WriteLine("Computer Choice Is {0}", pos);
-                Console.WriteLine("Player Choice Is O");
+                symbol = 'O';
             }
+            switch (position)
+            {
+                case 1:
+                    board[1] = symbol;
+                    break;
+                case 2:
+                    board[2] = symbol;
+                    break;
+                case 3:
+                    board[3] = symbol;
+                    break;
+                case 4:
+                    board[4] = symbol;
+                    break;
+                case 5:
+                    board[5] = symbol;
+                    break;
+                case 6:
+                    board[6] = symbol;
+                    break;
+                case 7:
+                    board[7] = symbol;
+                    break;
+                case 8:
+                    board[8] = symbol;
+                    break;
+                case 9:
+                    board[9] = symbol;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        public  string checkWinner(char [] board)
+        {
+           : for (int a = 0; a < 8; a++)
+            {
+                String line = null;
+
+                switch (a)
+                {
+                    case 0:
+                        line = "" + board[1] + board[2] + board[3];
+                        break;
+                    case 1:
+                        line = "" + board[4] + board[5] + board[6];
+                        break;
+                    case 2:
+                        line = "" + board[7] + board[8] + board[9];
+                        break;
+                    case 3:
+                        line = "" + board[1] + board[4] + board[7];
+                        break;
+                    case 4:
+                        line = "" + board[2] + board[5] + board[8];
+                        break;
+                    case 5:
+                        line = "" + board[3] + board[6] + board[9];
+                        break;
+                    case 6:
+                        line = "" + board[1] + board[5] + board[9];
+                        break;
+                    case 7:
+                        line = "" + board[3] + board[5] + board[7];
+                        break;
+                    default:
+                        break;
+
+                }
+                if (line.Equals("XXX"))
+                {
+                    return "X";
+                    Console.WriteLine("player Wins");
+                    
+
+
+                }
+                else if (line.Equals("OOO"))
+                {
+                    Console.WriteLine("Computer wins");
+                    return "O";
+                    break;
+
+
+                }
+
+            }
+            return "";
+
+
         }
     }
 }
