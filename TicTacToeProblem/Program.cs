cc37395f7899150d044a287eff09b91ddd793c6c
Prashant Bhure <Prashantbhure50@gmail.com>
2021-05-07 18:59:18 +0530

    [prashant]Display Board

--- TicTacToeProblem/Program.cs
+++ TicTacToeProblem/Program.cs
@@ -6,7 +6,14 @@ namespace TicTacToeProblem
     {
         static void Main(string[] args)
         {
-            
+            TicTacToe tic = new TicTacToe();
+            char[] board = new char[10];
+            Console.WriteLine("Welcome to TicTacToe Game");
+            tic.Board(board);
+            tic.DisplayBoard(board);
+            Console.WriteLine("Enter a Choice X or O");
+            string pos = Console.ReadLine();
+            tic.PlayerPositionCheck(pos);
         }
     }
 }
