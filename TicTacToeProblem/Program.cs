ff24cd29fb0b098edf962100ee355813e25f4dee
Prashant Bhure <Prashantbhure50@gmail.com>
2021-05-09 23:31:36 +0530

    [prashant]Refractor the code

--- TicTacToeProblem/Program.cs
+++ TicTacToeProblem/Program.cs
@@ -4,37 +4,45 @@ namespace TicTacToeProblem
 {
     class Program
     {
+        static int flag = 0;
+        static int player = 1;
         static void Main(string[] args)
         {
             TicTacToe tic = new TicTacToe();
-            char[] board = new char[10];
-            Random random = new Random();
+            char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
             Console.WriteLine("Welcome to TicTacToe Game");
-            tic.Board(board);
-            tic.DisplayBoard(board);
-            string Playerchoice = "X";
-            string computerChoice = "O";
-          
-          
-
-
-            Console.WriteLine("Enter a Choice Between  1-9");
-
-            while (true)
+            Console.Clear();
+            tic.DisplayBoard(board);         
+            while (flag != 1 && flag != -1)
             {
-                Console.WriteLine("   player Turn         ");
-                int position = Convert.ToInt32(Console.ReadLine());              
-                tic.PositionHolder(board, Playerchoice, position);
-                tic.DisplayBoard(board);
-                tic.checkWinner(board);
-                Console.WriteLine("   Computer Turn       ");
-                int computerTurn = random.Next(1,10);
-                tic.PositionHolder(board, computerChoice, computerTurn);
-                tic.DisplayBoard(board);
-                tic.checkWinner(board);
+                if (player % 2 == 0)
+                    Console.WriteLine("Player O Chance");
+                else
+                    Console.WriteLine("Player X Chance");
+
+                int position1 = Convert.ToInt32(Console.ReadLine());
+                player= tic.PositionHolder(board, player, position1);
+                Console.Clear();
+                tic.DisplayBoard(board);                
+                flag = tic.CheckWinner(board);             
             }
 
+            if (flag == 1)
+            {
+                Console.WriteLine("************Player 1 is winner*************");
+            }
+            else if (flag == -1)
+            {
+                Console.WriteLine("*************Player 2 is Winner***********");
+            }
+            else if(flag ==2)
+            {
+                Console.WriteLine("Game Draw");
+            }
 
+            Console.ReadKey();
         }
     }
-}
+
+}  
+
